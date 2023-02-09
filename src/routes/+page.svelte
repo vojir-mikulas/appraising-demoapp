3672d2d30ea47a64fbb13438d79c18fcc17e3d53
Mikuláš Vojíř <vojir.mikulas@gmail.com>
2023-02-09 14:41:42 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<style>
+
+    @import url('https://fonts.googleapis.com/css2?family=Mukta:wght@700&family=Roboto+Condensed:wght@700&display=swap');
+    h1{
+        font-size: 15vmin;
+        font-family: 'Mukta', sans-serif;
+        user-select: none;
+    }
+    main{
+        width: 100%;
+        height: 95vh;
+        display:grid;
+        place-items: center;
+    }
+</style>
+
+
+<main>
+    <h1>Appraising</h1>
+</main>
